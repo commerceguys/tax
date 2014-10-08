a46b2fe7e95ded0a813e7bb68e9095e1b932fb0e
Bojan Zivanovic <bojanz@gmail.com>
2014-10-08 22:12:24 +0200

    Separate tax rate amounts from tax rates.

---
+++ src/TaxRateAmountInterface.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace CommerceGuys\Tax;
+
+interface TaxRateAmountInterface
+{
+    /**
+     * Gets the tax rate.
+     *
+     * @return TaxRateInterface The tax rate.
+     */
+    public function getRate();
+
+    /**
+     * Sets the tax rate.
+     *
+     * @param TaxRateInterface $rate The tax rate.
+     */
+    public function setRate(TaxRateInterface $rate);
+
+    /**
+     * Gets the tax rate amount id.
+     *
+     * @return string The tax rate amount id.
+     */
+    public function getId();
+
+    /**
+     * Sets the tax rate amount id.
+     *
+     * @param string $id The tax rate amountid.
+     */
+    public function setId($id);
+
+    /**
+     * Gets the decimal tax rate amount.
+     *
+     * For example, 0.2 for a 20% tax rate.
+     *
+     * @return float The tax rate amount expressed as a decimal.
+     */
+    public function getAmount();
+
+    /**
+     * Sets the decimal tax rate amount.
+     *
+     * @param float $amount The tax rate amount expressed as a decimal.
+     */
+    public function setAmount($amount);
+
+    /**
+     * Gets the tax rate amount start date.
+     *
+     * @return \DateTime|null The tax rate amount start date, if known.
+     */
+    public function getStartDate();
+
+    /**
+     * Sets the tax rate amount start date.
+     *
+     * @param \DateTime $startDate The tax rate amount start date.
+     */
+    public function setStartDate($startDate);
+
+    /**
+     * Gets the tax rate amount end date.
+     *
+     * @return \DateTime|null The tax rate amount end date, if known.
+     */
+    public function getEndDate();
+
+    /**
+     * Sets the tax rate amount end date.
+     *
+     * @param \DateTime $endDate The tax rate amount end date.
+     */
+    public function setEndDate($endDate);
+}
