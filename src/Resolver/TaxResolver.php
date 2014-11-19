53a4c21ad845d34e0fbf510db7e37e5e7ecf1fff
Bojan Zivanovic <bojanz@gmail.com>
2014-11-19 15:34:21 +0100

    Add the TaxResolver facade.

---
+++ src/Resolver/TaxResolver.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace CommerceGuys\Tax\Resolver;
+
+use CommerceGuys\Tax\TaxableInterface;
+use CommerceGuys\Tax\Resolver\Engine\TaxRateResolverEngineInterface;
+use CommerceGuys\Tax\Resolver\Engine\TaxTypeResolverEngineInterface;
+
+class TaxResolver implements TaxResolverInterface
+{
+    /**
+     * The tax type resolver engine.
+     *
+     * @var TaxTypeResolverEngineInterface
+     */
+    protected $taxTypeResolverEngine;
+
+    /**
+     * The tax rate resolver engine.
+     *
+     * @var TaxRateResolverEngineInterface
+     */
+    protected $taxRateResolverEngine;
+
+    /**
+     * Creates a TaxResolver instance.
+     *
+     * @param TaxTypeResolverEngineInterface $taxTypeResolverEngine
+     * @param TaxRateResolverEngineInterface $taxRateResolverEngine
+     */
+    public function __construct($taxTypeResolverEngine, $taxRateResolverEngine)
+    {
+        $this->taxTypeResolverEngine = $taxTypeResolverEngine;
+        $this->taxRateResolverEngine = $taxRateResolverEngine;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolveAmounts(TaxableInterface $taxable, Context $context)
+    {
+        $date = $context->getDate();
+        $rates = $this->resolveRates($taxable, $context);
+        $amounts = array();
+        foreach ($rates as $rate) {
+            $amounts[] = $rate->getAmount($date);
+        }
+
+        return $amounts;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolveRates(TaxableInterface $taxable, Context $context)
+    {
+        $types = $this->resolveTypes($taxable, $context);
+        $rates = array();
+        foreach ($types as $type) {
+            $rate = $this->taxRateResolverEngine->resolve($type, $taxable, $context);
+            if ($rate) {
+                $rates[] = $rate;
+            }
+        }
+
+        return $rates;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolveTypes(TaxableInterface $taxable, Context $context)
+    {
+        return $this->taxTypeResolverEngine->resolve($taxable, $context);
+    }
+}
---
+++ tests/Resolver/TaxResolverTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace CommerceGuys\Tax\Tests\Resolver;
+
+use CommerceGuys\Tax\Resolver\TaxResolver;
+
+/**
+ * @coversDefaultClass \CommerceGuys\Tax\Resolver\TaxResolver
+ */
+class TaxResolverTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::__construct
+     */
+    public function testConstructor()
+    {
+        $taxTypeResolverEngine = $this
+            ->getMockBuilder('CommerceGuys\Tax\Resolver\Engine\TaxTypeResolverEngine')
+            ->getMock();
+        $taxRateResolverEngine = $this
+            ->getMockBuilder('CommerceGuys\Tax\Resolver\Engine\TaxRateResolverEngine')
+            ->getMock();
+        $resolver = new TaxResolver($taxTypeResolverEngine, $taxRateResolverEngine);
+        $this->assertSame($taxTypeResolverEngine, $this->getObjectAttribute($resolver, 'taxTypeResolverEngine'));
+        $this->assertSame($taxRateResolverEngine, $this->getObjectAttribute($resolver, 'taxRateResolverEngine'));
+
+    }
+
+    /**
+     * @covers ::resolveAmounts
+     * @covers ::resolveRates
+     * @covers ::resolveTypes
+     * @uses \CommerceGuys\Tax\Resolver\TaxResolver::__construct
+     */
+    public function testResolver()
+    {
+        $firstTaxRateAmount = $this
+            ->getMockBuilder('CommerceGuys\Tax\Model\TaxRateAmount')
+            ->getMock();
+        $secondTaxRateAmount = $this
+            ->getMockBuilder('CommerceGuys\Tax\Model\TaxRateAmount')
+            ->getMock();
+        $firstTaxRate = $this
+            ->getMockBuilder('CommerceGuys\Tax\Model\TaxRate')
+            ->getMock();
+        $firstTaxRate->expects($this->any())
+            ->method('getAmount')
+            ->will($this->returnValue($firstTaxRateAmount));
+        $secondTaxRate = $this
+            ->getMockBuilder('CommerceGuys\Tax\Model\TaxRate')
+            ->getMock();
+        $secondTaxRate->expects($this->any())
+            ->method('getAmount')
+            ->will($this->returnValue($secondTaxRateAmount));
+        $firstTaxType = $this
+            ->getMockBuilder('CommerceGuys\Tax\Model\TaxType')
+            ->getMock();
+        $secondTaxType = $this
+            ->getMockBuilder('CommerceGuys\Tax\Model\TaxType')
+            ->getMock();
+
+        $taxTypeResolverEngine = $this
+            ->getMockBuilder('CommerceGuys\Tax\Resolver\Engine\TaxTypeResolverEngine')
+            ->getMock();
+        $taxTypeResolverEngine->expects($this->any())
+            ->method('resolve')
+            ->will($this->returnValue(array($firstTaxType, $secondTaxType)));
+        $taxRateResolverEngine = $this
+            ->getMockBuilder('CommerceGuys\Tax\Resolver\Engine\TaxRateResolverEngine')
+            ->getMock();
+        $taxRateResolverEngine->expects($this->exactly(2))
+            ->method('resolve')
+            ->will($this->onConsecutiveCalls($firstTaxRate, $secondTaxRate));
+
+        $resolver = new TaxResolver($taxTypeResolverEngine, $taxRateResolverEngine);
+        $taxable = $this
+            ->getMockBuilder('CommerceGuys\Tax\TaxableInterface')
+            ->getMock();
+        $context = $this
+            ->getMockBuilder('CommerceGuys\Tax\Resolver\Context')
+            ->disableOriginalConstructor()
+            ->getMock();
+        // Since resolveAmounts calls resolveRates and resolveTypes, there
+        // is no need to invoke them separately.
+        $result = $resolver->resolveAmounts($taxable, $context);
+        $this->assertEquals(array($firstTaxRateAmount, $secondTaxRateAmount), $result);
+    }
+}
